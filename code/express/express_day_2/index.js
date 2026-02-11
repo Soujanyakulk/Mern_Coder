4ed8518a851eac5e3b32a251a86430aa671ebda0
Soujanya Kulkarni <soujanyakulkarni753@gmail.com>
2026-02-11 11:30:49 +0530

    Add express_day_3 folder

--- code/express/express_day_2/index.js
+++ code/express/express_day_2/index.js
@@ -1,56 +1,50 @@
-const express=require('express')
-const app=express()
-app.use(express.static('public/'))
-
-const menu=`
+const express = require("express");
+const app = express();
+app.use(express.static("public/"));
+const menu = `
 <ul>
 <li><a href='/'>Home</a><li>
 <li><a href='/about'>About</a><li>
 <li><a href='/contact'>Contact</a><li>
-</ul>`
-
-app.get('/',(req,res)=>{
-    res.send(menu+"<h1>Home Page</h1>")
-})
-
+</ul>`;
+app.get("/", (req, res) => {
+  res.send(menu + "<h1>Home Page</h1>");
+});
 // about us
-app.get('/about',(req,res)=>{
-    res.send(menu+`<h1>About us Page</h1>
+app.get("/about", (req, res) => {
+  res.send(
+    menu +
+      `<h1>About us Page</h1>
         <img src="download.jpg" alt="Laptop Wallpaper">
-        `)
-})
-
+        `,
+  );
+});
 // contact
-app.get('/contact',(req,res)=>{
-    // JSON Data Render
-    const obj={
-        id:1,
-        name:"Pratik",
-        email:"pratik@gmail.com",
-        address:'Pune'
-    }
-    res.send(obj)
-    // res.send(menu+"<h1>Contact Us</h1>")
-})
-
-
-app.get('/contact/contact1',(req,res)=>{
-    res.send("<h1>Contact Us 1 Page</h1>")
-})
-
-
+app.get("/contact", (req, res) => {
+  // JSON Data Render
+  const obj = {
+    id: 1,
+    name: "Pratik",
+    email: "pratik@gmail.com",
+    address: "Pune",
+  };
+  res.send(obj);
+  // res.send(menu+"<h1>Contact Us</h1>")
+});
+app.get("/contact/contact1", (req, res) => {
+  res.send("<h1>Contact Us 1 Page</h1>");
+});
 // middleware
 // fall back routing
-app.use((req,res)=>{
-    res.send(`
+app.use((req, res) => {
+  res.send(`
         <h1>404- Page Not Found</h1>
         <a href='/'>Go To Home Page</a>
         
-        `)
-    })
-
-PORT=3000
-HOST='127.0.0.1'
-app.listen(PORT,HOST,()=>{
+        `);
+});
+PORT = 3000;
+HOST = "127.0.0.1";
+app.listen(PORT, HOST, () => {
   console.log(`Server running on http://${HOST}:${PORT}`);
-})
+});
